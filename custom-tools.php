0270adb94f47e7b4add19902727c893b65f46a73
farizzo <git@fabriziorizzo.it>
2025-07-31 10:08:01 +0200

    feat: ACF settings exprot in JSON

--- custom-tools.php
+++ custom-tools.php
@@ -13,3 +13,25 @@ if (!defined('ABSPATH')) exit;
 // Includi i file delle funzionalità
 require_once plugin_dir_path(__FILE__) . 'inc/register-cpt.php';
 require_once plugin_dir_path(__FILE__) . 'inc/admin-columns.php';
+
+// require_once plugin_dir_path(__FILE__) . 'inc/acf-fields.php'; // se ti serve in futuro
+
+
+/**
+ * =======================================================
+ * ACF Export in Json
+ * =======================================================
+ * Esporta le impostazioni ACF in un file JSON così da
+ * archiviare tutto nella repo del progetto
+ */
+add_filter('acf/settings/save_json', function () {
+    return plugin_dir_path(__FILE__) . 'assets/acf-json';
+});
+
+add_filter('acf/settings/load_json', function ($paths) {
+    $paths[] = plugin_dir_path(__FILE__) . 'assets/acf-json';
+    return $paths;
+});
+
+
+
